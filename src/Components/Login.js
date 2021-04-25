a93103e68315d460fdf79055b9b8edd56e89046a
Samarth Gupta <samarth.gupta@teradata.com>
2021-04-25 21:59:20 +0530

    Made front page

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -11,85 +11,55 @@ import {
 } from "@ant-design/icons";
 
 import "./Login.css";
+import LocationSelector from "./LocationSelector";
 
 class Login extends React.Component {
+  state = { userType: null };
+
   tabList = [
     {
-      key: "tab1",
+      key: "traveller",
       tab: <UserOutlined style={{ fontSize: "32px", paddingLeft: "15px" }} />,
     },
     {
-      key: "tab2",
-      tab: <CarOutlined  style={{ fontSize: "32px", paddingLeft: "15px" }} />,
+      key: "cab",
+      tab: <CarOutlined style={{ fontSize: "32px", paddingLeft: "15px" }} />,
     },
   ];
+
+  onTabChange = (key) => {
+    console.log(key);
+    this.setState({ userType: key });
+  };
+
+  componentDidMount() {
+    // todo: send current location to websocket
+  }
 
   render() {
     return (
       <div className="login-container">
         <Card
-          style={{ width: 300 }}
-          // title="Hey, Welcome back!"
+          style={{ width: "50rem" }}
+          // title="Choose your role"
           tabList={this.tabList}
           hoverable
           headStyle={{ fontSize: "20px" }}
           // activeTabKey={this.state.key}
-          // onTabChange={key => {
-          //   this.onTabChange(key, 'key');
-          // }}>
+          onTabChange={(key) => {
+            this.onTabChange(key);
+          }}
           tabProps={{ centered: true }}
         >
-          <Input
-            size="large"
-            placeholder="Username"
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            // suffix={
-            //   <Tooltip title="Extra information">
-            //     <InfoCircleOutlined style={{ color: "rgba(0,0,0,.45)" }} />
-            //   </Tooltip>
-            // }
-          />
-          <br />
-          <br />
-          <Input.Password
-            placeholder="Password"
-            size="large"
-            prefix={<LockOutlined className="site-form-item-icon" />}
-          />
-          <br />
-
-          {/* <div className="forgot-password">
-            <Button
-              type="link"
-              size="small"
-              className="btn forgot-password-btn"
-            >
-              Forgot Password?
-            </Button>
-          </div> */}
-          <br />
+          <div className="location-selector">
+            <LocationSelector />
+          </div>
           <br />
           <div className="btn-container">
             <Button type="primary" size="large" className="btn signin">
-              Sign In
-            </Button>
-            <Button type="default" size="large" className="btn cancel">
-              Cancel
+              Let's Go!
             </Button>
           </div>
-
-          {/* <Divider>Or login with</Divider>
-          <div className="icons-container">
-            <a href="#" className="icon-link">
-              <GoogleOutlined style={{ fontSize: "32px" }} />
-            </a>
-            <a href="#" className="icon-link">
-              <FacebookOutlined style={{ fontSize: "32px" }} />
-            </a>
-            <a href="#" className="icon-link">
-              <TwitterOutlined style={{ fontSize: "32px" }} />
-            </a>
-          </div> */}
         </Card>
       </div>
     );
